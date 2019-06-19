1fa3a06c8c98a31d0de7fe8c81b2b76662abf6a2
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-06-19 08:38:46 -0700

    updated settings

--- webpack.dev.js
+++ webpack.dev.js
@@ -34,7 +34,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             inject: false,
             hash: false,
-            title: 'pxt-editor-extension-sample',
+            title: 'pxt-qrcode-generator',
             semanticUIPath: 'semantic-ui-css',
             reactPath: 'react',
             reactDomPath: 'react-dom',
