47408bc01ec967d628b515ef1c53d97554e27dfa
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-04-04 06:32:12 -0700

    udpated example

--- test.ts
+++ test.ts
@@ -1,24 +1,2 @@
-namespace test {
-
-    import QRCode = qrcode.QRCode;
-    import ErrorCorrectLevel = qrcode.ErrorCorrectLevel;
-    import QRNumber = qrcode.QRNumber;
-    import QRAlphaNum = qrcode.QRAlphaNum;
-    import QR8BitByte = qrcode.QR8BitByte;
-
-    export function run(): void {
-        let qr = new QRCode();
-        qr.setTypeNumber(5);
-        qr.setErrorCorrectLevel(ErrorCorrectLevel.H);
-        qr.addData(new QRNumber('0123')); // Number only
-        qr.addData(new QRAlphaNum('AB5678CD')); // Alphabet and Number
-        qr.addData(new QR8BitByte('[8BitByte :)]')); // most useful for usual purpose.
-        qr.make();
-
-        let img = qr.toImage();
-
-        let sprite = sprites.create(img);
-    }
-
-    run();
-}
+const ig = qrcode.encodeString("https://arcade.makecode.com");
+const sprite = sprites.create(ig);
